8d378342bdbf6db0fc337c75c3b1738f82cd48e5
bashokusan <pierresimonnet1@gmail.com>
2020-01-14 21:37:11 +0100

    test

--- libraries/autoload.php
+++ libraries/autoload.php
@@ -3,7 +3,7 @@
 function autoload($classname)
 {
     $classname = str_replace("\\", "/", $classname);
-    require_once "libraries/$classname.php";
+    require_once "$classname.php";
 }
 
 spl_autoload_register('autoload');
