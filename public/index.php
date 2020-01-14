64ff05202e93f0c46ae0d9785f82b5334a557321
bashokusan <pierresimonnet1@gmail.com>
2020-01-14 19:03:04 +0100

    testing

--- public/index.php
+++ public/index.php
@@ -1,7 +1,13 @@
-<?php
-session_start();
-
-require_once dirname(__DIR__).DIRECTORY_SEPARATOR."libraries/autoload.php";
-
-App::process();
-echo "coucou";
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>Bienvenue</h1>
+</body>
+</html>
